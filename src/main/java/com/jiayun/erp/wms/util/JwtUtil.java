94b666797e4c65a1138aaac0725d5a724c337d15
dotatata <dotatata@163.com>
2024-03-16 08:37:20 +0800

    开始添加JWT

---
+++ src/main/java/com/jiayun/erp/wms/util/JwtUtil.java
@@ -0,0 +1,73 @@
+package com.jiayun.erp.wms.util;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTCreator;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+
+    public static String secret = "abcdefghijklmnopqrstuvwxyz";
+
+    public static long time = 7 * 24 * 60 * 60 * 1000L;
+
+
+    public static String createTokenByMap(Map<String, String> map) {
+
+        JWTCreator.Builder builder = JWT.create();
+        map.forEach(builder::withClaim);
+
+        builder.withExpiresAt(new Date(System.currentTimeMillis() + time));
+
+        return builder.sign(Algorithm.HMAC256(secret));
+    }
+
+    public static String createToken(String key, String value){
+        JWTCreator.Builder builder = JWT.create();
+        builder.withClaim(key, value);
+        builder.withExpiresAt(new Date(System.currentTimeMillis() + time));
+
+        return builder.sign(Algorithm.HMAC256(secret));
+    }
+
+    public static String getClaim(String token, String key){
+        // 验证签名
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+        DecodedJWT decodedToken = verifier.verify(token);
+        return decodedToken.getClaim(key).asString();
+    }
+
+    public static DecodedJWT getDecodedJwt(String token){
+        // 验证签名
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+        return verifier.verify(token);
+    }
+
+    //public static Map<String, String> getTokenMap(String token, String key){
+    //    // 验证签名
+    //    JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+    //    DecodedJWT decodedToken = verifier.verify(token);
+    //    return decodedToken.getPayload();
+    //}
+
+    public static boolean isExpired(String token){
+        JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).build();
+        try{
+            DecodedJWT decodedToken = verifier.verify(token);
+            return true;
+        }catch (Exception e){
+            //TODO 打印错误日志
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+
+
+
+}
--- src/test/java/com/jiayun/erp/wms/WmsApplicationTests.java
+++ src/test/java/com/jiayun/erp/wms/WmsApplicationTests.java
@@ -1,13 +1,78 @@
 package com.jiayun.erp.wms;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.jiayun.erp.wms.entity.User;
+import com.jiayun.erp.wms.mapper.AuthMapper;
+import com.jiayun.erp.wms.mapper.UserMapper;
+import com.jiayun.erp.wms.util.JwtUtil;
+import com.jiayun.erp.wms.util.RedisUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class WmsApplicationTests {
 
+    @Autowired
+    RedisUtil redisUtil;
+    @Autowired
+    UserMapper userMapper;
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void redisTest(){
+        System.out.println("start time: " + System.currentTimeMillis());
+        String key = "user:auth";
+        redisUtil.set(key, 1);
+
+        int value = (int) redisUtil.get(key);
+        System.out.println("key '" + key + "' value is " + value);
+        System.out.println("end time: " + System.currentTimeMillis());
+
+    }
+
+    @Test
+    void jwtTest(){
+        System.out.println("start time: " + System.currentTimeMillis());
+
+        User user = userMapper.getUserWithRolesByPhone("13888888888");
+
+
+        Map<String, String> map = new HashMap<>();
+        map.put("uid", user.getId().toString());
+        map.put("phone", user.getPhone());
+        map.put("name", user.getName());
+        map.put("roles", user.getRoles().toString());
+        //map.put("permissions", user.getName());
+
+        String token = JwtUtil.createTokenByMap(map);
+        System.out.println("JWT token: " + token);
+
+
+        System.out.println("==========decode token==========");
+        DecodedJWT decodedToken = JwtUtil.getDecodedJwt(token);
+        System.out.println("decodedToken: " + decodedToken);
+        System.out.println("getHeader: " + decodedToken.getHeader());
+        System.out.println("getPayload: " + decodedToken.getPayload());
+        System.out.println("getToken: " + decodedToken.getToken());
+        System.out.println("getSignature: " + decodedToken.getSignature());
+        System.out.println("claim 'uid': " + decodedToken.getClaim("uid"));
+        System.out.println("claim 'phone': " + decodedToken.getClaim("phone"));
+        System.out.println("claim 'roles': " + decodedToken.getClaim("roles"));
+
+
+        System.out.println("end time: " + System.currentTimeMillis());
+
     }
 
 }
